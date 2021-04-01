d04a6383f0ed26ff0b8753d0e6d68ec730a16aa6
iL53n <ilsegribko@gmail.com>
2021-04-01 23:02:51 +0300

    feat(app): add Users::CreateService

---
+++ app/models/user_session.rb
@@ -0,0 +1,9 @@
+class UserSession < Sequel::Model
+
+  many_to_one :user
+
+  def validate
+    super
+    validates_presence :uuid, message: I18n.t(:blank, scope: 'model.errors.user_session.uuid')
+  end
+end
